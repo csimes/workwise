1f771cf4e65374073261c66cb859877f8c45dca1
csimes <cherron@developdbycherron.com>
2024-04-08 15:39:38 -0500

    basic login and register page setup to test that routes are working

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,15 @@
+import { Button, Input } from "@chakra-ui/react";
+
+const Register = () => {
+  return (
+    <div>
+      <h1>Register</h1>
+      <Input
+        placeholder="Email"
+        type="email"
+      />
+    </div>
+  );
+};
+
+export default Register;
