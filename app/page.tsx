bcd4b05bbf649578f4375b1e6dfe55832f7e3e6c
csimes <cherron@developdbycherron.com>
2024-04-08 15:34:06 -0500

    add basic home page setup with buttons that route to register and login pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,22 @@
 "use client";
 import Image from "next/image";
 import { Link } from "@chakra-ui/next-js";
+import { Button } from "@chakra-ui/react";
 
 export default function Home() {
   return (
-    <Link
-      href="/dashboard/settings"
-      color="blue.400"
-      _hover={{ color: "blue.500" }}
-    >
-      About
-    </Link>
+    <div className="text-center m-auto">
+      <title>WorkWise: Home</title>
+      <h1>Welcome to WorkWise!</h1>
+      <p>Track your time and gain insights into your workday.</p>
+      <div>
+        <Button>
+          <Link href="auth/login">Login</Link>
+        </Button>
+        <Button>
+          <Link href="auth/register">Register</Link>
+        </Button>
+      </div>
+    </div>
   );
 }
