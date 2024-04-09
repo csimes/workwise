ff65e474df08bdd9904fca37db16c3718e4cc8ff
csimes <cherron@developdbycherron.com>
2024-04-08 22:38:41 -0500

    add form labels and sign up button to register form

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,20 +1,28 @@
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Input, FormControl, FormLabel } from "@chakra-ui/react";
 
 const Register = () => {
   return (
     <div>
       <h1>Create an account</h1>
-      <Input placeholder="First name" />
-      <Input placeholder="Last name" />
-      <Input
-        placeholder="jane.doe@email.com"
-        type="email"
-      />
-      <Input placeholder="Doeadeer" />
-      <Input
-        placeholder="Enter at least 8+ characters"
-        type="password"
-      />
+      <FormControl>
+        <FormLabel>First name</FormLabel>
+        <Input placeholder="First name" />
+        <FormLabel>Last name</FormLabel>
+        <Input placeholder="Last name" />
+        <FormLabel>Email address</FormLabel>
+        <Input
+          placeholder="jane.doe@email.com"
+          type="email"
+        />
+        <FormLabel>Username</FormLabel>
+        <Input placeholder="Doeadeer" />
+        <FormLabel>Password</FormLabel>
+        <Input
+          placeholder="Enter at least 8+ characters"
+          type="password"
+        />
+        <Button>Sign Up</Button>
+      </FormControl>
     </div>
   );
 };
