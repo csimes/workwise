5b98e9b7ce44ee1acc8afd91194efdb52358c897
csimes <cherron@developdbycherron.com>
2024-04-08 22:20:38 -0500

    add remaining account registration fields

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -3,10 +3,17 @@ import { Button, Input } from "@chakra-ui/react";
 const Register = () => {
   return (
     <div>
-      <h1>Register</h1>
+      <h1>Create an account</h1>
+      <Input placeholder="First name" />
+      <Input placeholder="Last name" />
       <Input
-        placeholder="Email"
+        placeholder="jane.doe@email.com"
         type="email"
+      />
+      <Input placeholder="Doeadeer" />
+      <Input
+        placeholder="Enter at least 8+ characters"
+        type="password"
       />
     </div>
   );
